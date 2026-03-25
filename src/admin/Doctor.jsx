222f07f777280031decd1413c36f61fc2d70323f
Prince <princemungra1340@gmail.com>
2026-03-25 14:28:53 +0530

    update page

--- src/admin/Doctor.jsx
+++ src/admin/Doctor.jsx
@@ -109,8 +109,16 @@ function Doctors() {
 
 
 
-                        <button
+                        {/* <button
                           onClick={() => navigate(`/admin/doctors/edit/${doc.user._id}`)}
+                          className="px-4 py-2 bg-blue-500 text-white rounded-lg"
+                        >
+                          Edit
+                        </button> */}
+
+
+                        <button
+                          onClick={() => navigate(`/admin/doctors/edit/${doc._id}`)}
                           className="px-4 py-2 bg-blue-500 text-white rounded-lg"
                         >
                           Edit
